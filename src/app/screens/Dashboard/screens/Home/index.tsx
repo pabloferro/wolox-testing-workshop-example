4b10189f08b40e69dc2290d31df7e955b4909e2a
Henry Zarza <henryacz7@gmail.com>
2020-11-02 13:16:43 -0500

    Home layout

--- src/app/screens/Dashboard/screens/Home/index.tsx
+++ src/app/screens/Dashboard/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import i18next from 'i18next';
 
 import withProvider from '~components/ProviderWrapper';
 import { actionCreators as authActions } from '~contexts/UserContext/reducer';
@@ -7,16 +8,28 @@ import { logout, removeCurrentUser } from '~services/AuthServices';
 import { useLazyRequest } from '~app/hooks/useRequest';
 import Loading from '~components/Spinner/components/loading';
 
+import Card from './components/Card';
 import styles from './styles.module.scss';
-import { useSelector, Context, useDispatch } from './context';
+import { Context, useDispatch } from './context';
 import { reducer, INITIAL_STATE, actionCreators } from './reducer';
+
+const character = {
+  id: 4,
+  name: 'Batman',
+  photo: 'https://res.cloudinary.com/henryzarza/image/upload/v1566430490/DC%20Comics/Batman_g21b7m.jpg',
+  realName: 'Bruce Wayne',
+  description:
+    'Batman has dedicated his life to an endless crusade, a war on all criminals in the name of his murdered parents, who were taken from him when he was just a child.',
+  height: '1.88',
+  weight: '95'
+};
 
 function Home() {
   // Example of how to use these custom hooks
-  const foo = useSelector(state => state.foo);
+  // const foo = useSelector(state => state.foo);
   const dispatch = useDispatch();
   const userDispatch = useUserDispatch();
-  const [, , , logoutRequest] = useLazyRequest({
+  const [, loading, , logoutRequest] = useLazyRequest({
     request: logout,
     withPostSuccess: () => {
       userDispatch(authActions.resetUser());
@@ -33,16 +46,20 @@ function Home() {
     logoutRequest(null);
   };
 
-  return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <Loading className="m-bottom-4" />
-        <p className={styles.text}>You are logged in. {foo}</p>
-        <button type="button" className={styles.appLink} onClick={handleLogout}>
-          Logout
-        </button>
-      </header>
-    </div>
+  return loading ? (
+    <Loading />
+  ) : (
+    <main className="column center">
+      <div className={`row center wrap ${styles.characterContainer}`}>
+        <Card data={character} />
+        <Card data={character} />
+        <Card data={character} />
+        <Card data={character} />
+      </div>
+      <button type="button" className="button secondary small-title m-bottom-4" onClick={handleLogout}>
+        {i18next.t('Home:watchMore')}
+      </button>
+    </main>
   );
 }
 
