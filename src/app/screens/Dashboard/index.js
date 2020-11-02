1835f746866a1974c0339f9407cf984d4580d9c7
Henry Zarza <henryacz7@gmail.com>
2020-11-02 14:07:42 -0500

    Navbar component

--- src/app/screens/Dashboard/index.js
+++ src/app/screens/Dashboard/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import RouteItem from '~components/Routes/components/RouteItem';
+import Navbar from '~components/Navbar';
 import PATHS from '~components/Routes/paths';
 
 import Home from './screens/Home';
 
 function Dashboard() {
   return (
-    <Switch>
-      <RouteItem exact path={PATHS.home} component={Home} />
-    </Switch>
+    <>
+      <Navbar />
+      <Switch>
+        <RouteItem exact path={PATHS.home} component={Home} />
+      </Switch>
+    </>
   );
 }
 
