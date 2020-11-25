4f8a8779eb0cc4567d46e25598ee685d4f997d5e
Pablo Ferro <pablo.pyp@gmail.com>
2020-11-25 15:08:02 -0300

    Fix setuptest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,5 @@ import '@testing-library/jest-dom';
 import 'mutationobserver-shim';
 
 jest.mock('i18next', () => ({
-  t: (key: string, params: Record<string, string>) => `${key} ${JSON.stringify(params)}`
+  t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key)
 }));
