da20c312342779450de2e97ea8e057013b5bcf95
Pablo Ferro <pablo.pyp@gmail.com>
2020-11-26 10:25:13 -0300

    Change ???

--- src/app/screens/Dashboard/screens/Home/components/Card/index.tsx
+++ src/app/screens/Dashboard/screens/Home/components/Card/index.tsx
@@ -26,7 +26,7 @@ function Card({ data }: Props) {
       <img className={styles.cardImage} src={data.photo} alt={name} />
       <div className={`column ${styles.cardContent}`}>
         <h3 className="small-title white-color">{data.name}</h3>
-        <h6 className="base-text fw-bold blue-color">{showSpoilers ? data.realName : '????????'}</h6>
+        <h6 className="base-text fw-bold blue-color">{showSpoilers ? data.realName : '???'}</h6>
         <p className="small-text light-gray-color m-bottom-2">{data.description}</p>
         <div className={`row ${styles.characterInfo}`}>
           <span className="small-text white-color m-right-5">{data.weight} kg</span>
