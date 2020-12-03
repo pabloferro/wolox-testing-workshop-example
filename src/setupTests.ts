1723fd456962544ef5969d7b9a5dba32a3d3c385
Pablo Ferro <pablo.pyp@gmail.com>
2020-12-03 12:34:02 -0300

    Add Greetings and toTitleCase tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import 'mutationobserver-shim';
 
-// jest.mock('i18next', () => ({
-//   t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key)
-// }));
+jest.mock('i18next', () => ({
+  t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key)
+}));
